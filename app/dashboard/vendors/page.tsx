f9f57431291f3e56b239f57e7be2aecec893b8b8
Kave McDaniel <kave.mdi@gmail.com>
2025-05-06 06:58:48 +0100

    refactor vendors

--- app/dashboard/vendors/page.tsx
+++ app/dashboard/vendors/page.tsx
@@ -9,25 +9,25 @@ import { useEffect, useState } from "react";
 
 export default function VendorsPage() {
   const [isLoading, setLoading] = useState(true);
-  const vendorDocs = useQuery(api.vendors.getAllVendors, {});
+  const vendors = useQuery(api.vendors.getAllVendors, {});
   
   useEffect(() => {
     // Only set loading to false when vendorDocs is no longer undefined
-    if (vendorDocs !== undefined) {
+    if (vendors !== undefined) {
       setLoading(false);
     }
-  }, [vendorDocs]);
+  }, [vendors]);
 
   return (
     <div className="space-y-8">
-      <VendorSectionCards vendors={vendorDocs} isLoading={isLoading} />
+      <VendorSectionCards vendors={vendors} isLoading={isLoading} />
       {isLoading ? (
         <div className="px-4 lg:px-6 space-y-3">
           <Skeleton className="h-10 w-full" />
           <Skeleton className="h-96 w-full" />
         </div>
       ) : (
-        <DataTable data={vendorDocs || []} />
+        <DataTable data={vendors || []} />
       )}
     </div>
   );
